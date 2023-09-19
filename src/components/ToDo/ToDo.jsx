58819507447183b2e7e195de48b41e00b56f82c1
Bernardo Eliezer <berbuzon@gmail.com>
2023-09-19 14:50:06 -0300

    Agrego tareas

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 // estado = tareas[]
 // estado = forms{} (si lo hago controlado)
 // event submit que agregue tarea a [tareas]
+
+const tareas = [];
 
 /* [].map((tarea) => {
   return (
@@ -13,45 +15,62 @@ import React from "react";
     }); */
 
 const ToDo = () => {
+  const [tareas, setTareas] = useState([]);
+
+  const [entrada, setEntrada] = useState("");
+
+  const handleAddTask = () => {
+    setTareas([...tareas, entrada]);
+    // setEntrada("");
+    console.log(tareas);
+  };
+
   return (
     <div>
       <h1>To Do List</h1>
       <section>
         <h2>List</h2>
 
-    {
-        //tareas.map(tarea)=>{}
-    }
-        
         <ul>
-          <div
-            style={{
-              display: "flex",
-              width: "300 px",
-              justifyContent: "space-between",
-              border: "1px solid white",
-              borderRadius: "5px",
-              padding: "5px",
-            }}
-          >
-            <input type="checkbox" name="" id="" />
-            <li>item1</li>
+          {tareas.map((tarea, index) => (
             <div
+              key={index}
               style={{
                 display: "flex",
-                flexDirection: "column",
+                width: "300 px",
+                justifyContent: "space-between",
+                border: "1px solid white",
+                borderRadius: "5px",
+                padding: "5px",
               }}
             >
-              <button>Delete</button>
-              <button>Edit</button>
+              <input type="checkbox" name="" id="" />
+              <li>{tarea}</li>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <button>Delete</button>
+                <button>Edit</button>
+              </div>
             </div>
-          </div>
+          ))}
         </ul>
       </section>
       <section>
         <form>
-          <input type="text" name="" id="" />
-          <button>Add</button>
+          <input
+            type="text"
+            name="entrada"
+            id=""
+            value={entrada}
+            onChange={(e) => setEntrada(e.target.value)}
+          />
+          <button type="button" onClick={handleAddTask}>
+            Add
+          </button>
         </form>
       </section>
     </div>
